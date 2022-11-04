334550b173ffb7f48e2daa8e00f87f685c97f1e6
Şenay ŞENTÜRK <senaysenturk@outlook.com>
2022-11-05 00:36:14 +0300

    hermes-solutions

---
+++ src/js/dom.js
@@ -0,0 +1,17 @@
+const headerElement = document.querySelector("header");
+console.log(headerElement);
+
+const rowElement = document.querySelector(".row");
+console.log(rowElement);
+
+const allPTags = document.querySelectorAll("p");
+console.log(allPTags);
+
+const allH3Tags = document.querySelectorAll("h3");
+allH3Tags.forEach((h3) => console.log(h3));
+
+const allFlexGrid = document.getElementsByClassName("flex-grid");
+console.log(allFlexGrid);
+
+const solutionsElement = document.getElementById("solutions");
+console.log(solutionsElement);
